74a260f12ff540d495f1b2e7723016f3f6f9bb43
RiskyF <riskyfebri.rf@gmail.com>
2026-07-24 08:31:14 +0700

    Use Streamlit Secrets

--- ListPengeluaran.py
+++ ListPengeluaran.py
@@ -9,7 +9,8 @@ import math
 # KONEKSI GOOGLE SHEETS
 # ==========================
 SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets"
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
 ]
 
 creds = Credentials.from_service_account_info(
